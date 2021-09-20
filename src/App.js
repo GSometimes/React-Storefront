a3adea10056d2786a8450b398cfe114efd39cbe3
Gourav <gore.auluck@gmail.com>
2021-09-19 21:57:26 -0700

    Basic Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <p>Navigation</p>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/products/:handle">
+            <ProductPage />
+          </Route>
+        </Switch>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react";
+import { Link } from 'react-router-dom'
 import { ShopContext } from "../context/shopContext";
 
 const Home = () => {
@@ -13,7 +14,7 @@ const Home = () => {
   return (
     <div>
       {products.map((product) => (
-        <h1>{product.title}</h1>
+        <Link to={`/products/${product.handle}`} key={product.title}>{product.title}</Link>
       ))}
     </div>
   );
